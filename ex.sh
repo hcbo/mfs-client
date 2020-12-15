464de5440f9b9491c322c63389563ce58d50f2bc
hcb <1298881987@qq.com>
2020-12-15 14:00:32 +0800

    upload to private repo

--- ex.sh
+++ ex.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 mvn clean;
-mvn package -Dmaven.test.skip=true;
-jar uf target/hcbMfs-1.0.0.jar core-site.xml;
+#mvn package -Dmaven.test.skip=true;
+#jar uf target/hcbMfs-1.0.0.jar core-site.xml;
+# upload to private nexus repo
+mvn deploy -Dmaven.javadoc.skip=true
